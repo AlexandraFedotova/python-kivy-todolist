21d6707e1910354b7ba40cfa52da077f445a4307
alexandra_fedotova <alexandra.fedotova@dell.com>
2022-05-27 22:42:13 +0300

    It is temporarely result. Task have id, name, description, is_done and category. But I have a very big problems with models work

--- main.py
+++ main.py
@@ -3,7 +3,11 @@ from model import *
 from kivy.properties import ObjectProperty
 from kivymd.uix.screen import MDScreen
 from kivy.uix.screenmanager import ScreenManager
-from kivymd.uix.list import TwoLineIconListItem
+from kivymd.uix.list import TwoLineIconListItem, OneLineRightIconListItem, OneLineAvatarIconListItem
+from kivymd.uix.dialog import MDDialog
+from kivymd.uix.button import MDFlatButton
+from kivymd.uix.textfield import MDTextField
+from kivymd.uix.boxlayout import MDBoxLayout
 
 from kivy.core.window import Window
 
@@ -14,6 +18,92 @@ notebook = Notebook()
 
 class MainApp(ScreenManager):
     pass
+
+
+class CategoryItem(OneLineRightIconListItem):
+    def __init__(self, category_name, **kwargs):
+        super(CategoryItem, self).__init__(**kwargs)
+        self.text = category_name
+
+
+class CategoriesListScreen(MDScreen):
+    categories_list = ObjectProperty()
+    dialog = None
+
+    def delete_category(self, obj=None):
+        try:
+            notebook.remove_category(name_of_required_category=self.category_for_deleting)
+            self.set_categories_list()
+            self.close_dialog()
+        except Exception:
+            print('Error with deleting category')
+
+    def show_dialog_delete_category(self, instance):
+        self.category_for_deleting = instance.text
+        close_button = MDFlatButton(text='CLOSE', on_release=self.close_dialog)
+        remove_button = MDFlatButton(text='REMOVE', on_release=self.delete_category)
+        self.dialog = MDDialog(
+            title='Удалить категорию?',
+            text=instance.text,
+            buttons=[close_button, remove_button]
+        )
+        self.dialog.open()
+
+    def show_dialog_create_category(self):
+        close_button = MDFlatButton(text='CLOSE', on_release=self.close_dialog)
+        self.create_button = MDFlatButton(text='CREATE', on_release=self.create_category)
+        content = MDBoxLayout(orientation='vertical', size_hint_y=None, height='50sp')
+        self.text_field = MDTextField(hint_text='category name')
+        content.add_widget(self.text_field)
+        self.dialog = MDDialog(
+            title='Create category',
+            type="custom",
+            #size_hint=(0.8, 1),
+            content_cls=content,
+            buttons=[close_button, self.create_button],
+        )
+        self.dialog.open()
+
+    def close_dialog(self, obj=None):
+        try:
+            self.dialog.dismiss()
+        except Exception:
+            print('dialog dismiss failed ')
+
+    def create_category(self, obj=None):
+        if self.text_field.text.strip() == '':
+            self.text_field.hint_text = 'Plese enter a category name!'
+        else:
+            notebook.create_category(category_name=self.text_field.text)
+            self.set_categories_list()
+            self.close_dialog()
+
+    def set_categories_list(self):
+        self.categories_list.clear_widgets()
+        for category_name in notebook.get_categories_list():
+            self.categories_list.add_widget(CategoryItem(category_name))
+
+    def __init__(self, **kwargs):
+        super(CategoriesListScreen, self).__init__(**kwargs)
+
+
+class TaskCreationScreen(MDScreen):
+    button_task_creation = ObjectProperty()
+    textfield_task_name = ObjectProperty()
+    textfield_task_description = ObjectProperty()
+
+    def create_task(self, category):
+        task_name = self.textfield_task_name.text
+        task_description = self.textfield_task_description.text
+        notebook.add_task(Task(name=task_name, description=task_description, category=category))
+
+        self.textfield_task_name.set_text(self, text='')
+        self.textfield_task_description.set_text(self, text='')
+        self.button_task_creation.disabled = True
+        notebook.show_notebook()
+
+    def __init__(self, **kwargs):
+        super(TaskCreationScreen, self).__init__(**kwargs)
 
 
 class TaskItem(TwoLineIconListItem):
@@ -33,48 +123,30 @@ class TaskItem(TwoLineIconListItem):
         self.checkbox.state = 'normal' if task_is_done is False else 'down'
 
 
-class TaskCreationScreen(MDScreen):
-    button_task_creation = ObjectProperty()
-    textfield_task_name = ObjectProperty()
-    textfield_task_description = ObjectProperty()
-
-    def create_task(self):
-        task_name = self.textfield_task_name.text
-        task_description = self.textfield_task_description.text
-        notebook.add_task(Task(name=task_name, description=task_description))
-
-        self.textfield_task_name.set_text(self, text='')
-        self.textfield_task_description.set_text(self, text='')
-        self.button_task_creation.disabled = True
-
-    def __init__(self, **kwargs):
-        super(TaskCreationScreen, self).__init__(**kwargs)
-
-
-class TaskListScreen(MDScreen):
+class TasksInCategoryScreen(MDScreen):
     task_list = ObjectProperty()
+    toolbar = ObjectProperty()
 
-    def set_tasks_list(self):
+    def set_tasks_list(self, category):
+        self.toolbar.title= category
         self.task_list.clear_widgets()
 
-        for task_item in notebook.get_tasks():
+        for task_item in notebook.get_tasks_from_category(category):
+        #  for task_item in notebook.get_tasks():
             self.task_list.add_widget(TaskItem(task_name=task_item.get_name(),
                                                task_description=task_item.get_description(),
                                                task_is_done=task_item.is_done()))
-
-    def delete_item(self, instance):
-        task_name = instance.task_item.text
-        notebook.remove_task_by_name(task_name)
-        self.set_tasks_list()
+        notebook.show_notebook()
 
     def __init__(self, **kwargs):
-        super(TaskListScreen, self).__init__(**kwargs)
+        super(TasksInCategoryScreen, self).__init__(**kwargs)
 
 
 class TaskOnlyScreen(MDScreen):
     task_name = ObjectProperty()
     task_description = ObjectProperty()
     task_is_done = ObjectProperty()
+    task_category = ObjectProperty()
 
     def set_name(self):
         new_task_name = self.task_name.text
@@ -93,16 +165,23 @@ class TaskOnlyScreen(MDScreen):
         else:
             notebook.mark_task_undone(self.task_name_history)
 
-    def remove_task(self):
-        notebook.remove_task_by_name(self.task_name_history)
+    # todo - сделать какую-нибудь конечную проверку категории, а не сразу менять? 
+    def set_category(self):
+        print('set category')
+        new_category = self.task_category.text
+        notebook.set_task_category(self.task_name_history, new_category)
 
     def build(self, instance):
         self.task_name_history = instance.text
         task_description = instance.secondary_text
         task_is_done = True if instance.checkbox.state == 'down' else False
+        task_category = notebook.get_task_by_name(self.task_name_history).get_category()
+        print(notebook.get_task_by_name(self.task_name_history).show_task())
+        print("category " + task_category)
         self.task_name.text = self.task_name_history
         self.task_description.text = task_description
         self.task_is_done.active = task_is_done
+        self.task_category.text = task_category
 
     def __init__(self, instance, **kwargs):
         super(TaskOnlyScreen, self).__init__(**kwargs)
@@ -111,6 +190,8 @@ class TaskOnlyScreen(MDScreen):
 
 
 class MyApp(MDApp):
+    dialog = None
+
     def set_screen(self, screen_name):
         if self.screen_manager.has_screen(screen_name):
             self.screen_manager.current = screen_name
@@ -123,31 +204,41 @@ class MyApp(MDApp):
     def remove_task_only_screen(self):
         if self.screen_manager.has_screen('taskOnly'):
             screen = self.screen_manager.get_screen('taskOnly')
-            self.set_screen('taskList')
+            self.set_screen('tasksInCategory')
             self.screen_manager.remove_widget(screen)
 
     def remove_task(self, instance):
         task_name = instance.task_name.text
         notebook.remove_task_by_name(task_name)
         self.remove_task_only_screen()
+        notebook.show_notebook()
+
+    def show_tasks_in_categories_screen(self, category):
+        self.current_category = category
+        self.set_screen('tasksInCategory')
+        print(self.current_category)
 
     def on_start(self):
-        self.taskListScreen.set_tasks_list()
+        self.categoriesListScreen.set_categories_list()
+        #self.tasksInCategoryScreen.set_tasks_list(self.current_category)
 
     def on_stop(self):
         notebook.write_tasks_in_file()
 
     def __init__(self, **kwargs):
         self.screen_manager = None
-        self.taskListScreen = None
+        self.categoriesListScreen = None
+        self.tasksInCategoryScreen = None
         self.taskCreationScreen = None
+        self.current_category = 'Base'
         super(MyApp, self).__init__(**kwargs)
 
     def build(self):
         notebook.read_tasks_from_file()
         self.title = "Task list"
         self.screen_manager = MainApp()
-        self.taskListScreen = self.screen_manager.get_screen('taskList')
+        self.categoriesListScreen = self.screen_manager.get_screen('categoriesList')
+        self.tasksInCategoryScreen = self.screen_manager.get_screen('tasksInCategory')
         self.taskCreationScreen = self.screen_manager.get_screen('taskCreation')
 
         return self.screen_manager
--- model.py
+++ model.py
@@ -1,23 +1,34 @@
+import datetime
 import json
+import time
 
 
 class Task:
-    def __init__(self, name, description, is_done=False):
-        self.__name = name
-        self.__description = description
-        self.__is_done = is_done
+    # todo: add descriptions
+    def __init__(self, name, description, is_done=False, category='Base'):
+        self.__id = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
+        self.__name = str(name).strip()
+        self.__description = str(description).strip()
+        self.__is_done = is_done if type(is_done) is bool else False
+        self.__category = str(category).strip()
+
+    def get_id(self):
+        return self.__id
+
+    def set_id(self, new_id):
+        self.__id = str(new_id).strip()
 
     def get_name(self):
-        return str(self.__name)
+        return self.__name
 
     def set_name(self, new_name):
-        self.__name = new_name
+        self.__name = str(new_name).strip()
 
     def get_description(self):
         return self.__description
 
     def set_description(self, new_description):
-        self.__description = new_description
+        self.__description = str(new_description).strip()
 
     def is_done(self):
         return self.__is_done
@@ -28,70 +39,227 @@ class Task:
     def mark_undone(self):
         self.__is_done = False
 
+    def get_category(self):
+        return self.__category
+
+    def set_category(self, new_category):
+        self.__category = str(new_category).strip()
+
     def show_task(self):
-        description = "Task name: " + self.__name + ", is done: " + str(self.__is_done) + \
-                      ", task description: " + self.__description
+        description = "Task id: " + self.__id + ", task name: " + self.__name + ", is done: " + str(self.__is_done) + \
+                      ", task description: " + self.__description + ", task category: " + \
+                        self.__category
         return description
 
     def get_task_data(self):
         data_task = {
-            "name": self.__name,
+            "id"         : self.__id,
+            "name"       : self.__name,
             "description": self.__description,
-            "is_done": self.__is_done,
+            "is_done"    : self.__is_done,
+            "category"   : self.__category
         }
         return data_task
 
 
 class Notebook:
+    # todo: add descriptions
+    # todo: check types
+    # todo: add - str(something).strip() to every important property
     def __init__(self):
         self.__tasks = []
+        self.__categories_names = []
+        self.__tasks_ids_in_categories = {}
 
-    def is_task(self, task):
+    def is_task(self, obj):
         result = False
-        if type(task) is Task:
+        if type(obj) is Task:
             result = True
         return result
+
+    def create_category(self, category_name):
+        category_name = str(category_name).strip()
+        if category_name not in self.__categories_names and not "":
+            print('category creation: ' + category_name)
+            self.__categories_names.append(category_name)
+            self.__tasks_ids_in_categories[category_name] = []
+        print(self.__categories_names)
+
+    def get_categories_list(self):
+        return self.__categories_names
+
+    def get_tasks_from_category(self, name_of_required_category):
+        print("-----------------------------------")
+        print('get tasks from category')
+        # todo: add tasks sorting
+        tasks = []
+        name_of_required_category = str(name_of_required_category).strip()
+        print('name of required category ' + name_of_required_category)
+        if name_of_required_category in self.__categories_names:
+            print(self.__tasks_ids_in_categories[name_of_required_category])
+            for task_id in self.__tasks_ids_in_categories[name_of_required_category]:
+                print(task_id)
+                task = self.get_task_by_id(task_id)
+                if self.is_task(task):
+                    tasks.append(task)
+        print("==============================")
+        return tasks
+
+    def set_task_category(self, id_of_required_task, name_of_required_category):
+        print("-----------------------------------")
+        print('set_task_category')
+        name_of_required_category = str(name_of_required_category).strip()
+        id_of_required_task = str(id_of_required_task).strip()
+
+        task = self.get_task_by_id(id_of_required_task)
+        if self.is_task(task):
+            old_category = task.get_category()
+            if old_category == name_of_required_category:
+                return
+            task.set_category(name_of_required_category)
+            self.__tasks_ids_in_categories[old_category].remove(id_of_required_task)
+            self.create_category(name_of_required_category)
+            self.__tasks_ids_in_categories[name_of_required_category].append(id_of_required_task)
+        print("\nSet task " + id_of_required_task + " with name " + task.get_name() + " to category " + name_of_required_category)
+        print(self.__categories_names)
+        print(self.__tasks_ids_in_categories)
+        print("==============================")
+
+    def rename_category(self, name_of_required_category, new_category_name):
+        name_of_required_category = str(name_of_required_category).strip()
+
+        tasks = self.get_tasks_from_category(name_of_required_category)
+        for task in tasks:
+            self.set_task_category(task.get_id(), new_category_name)
+
+        self.__tasks_ids_in_categories.pop(name_of_required_category)
+        self.__categories_names.remove(name_of_required_category)
+        if new_category_name not in self.__categories_names:
+            self.__categories_names.append(new_category_name)
+            self.__tasks_ids_in_categories[new_category_name] = []
+
+    def remove_category(self, name_of_required_category, name_of_category_to_transfer_tasks_from_deleted=None):
+        name_of_required_category = str(name_of_required_category).strip()
+        name_of_category_to_transfer_tasks_from_deleted = str(name_of_category_to_transfer_tasks_from_deleted).strip()
+
+        self.__categories_names.remove(name_of_required_category)
+        self.__tasks_ids_in_categories.pop(name_of_required_category)
+        tasks = self.get_tasks_from_category(name_of_required_category)
+
+        if name_of_category_to_transfer_tasks_from_deleted is None:
+            print('name of category to transfer tasks from deleted is None ')
+            for task in tasks:
+                self.remove_task_by_id(task.get_id())
+        else:
+            print('name of category to transfer tasks from deleted is ' + name_of_category_to_transfer_tasks_from_deleted)
+            for task in tasks:
+                self.set_task_category(task.get_id(), name_of_category_to_transfer_tasks_from_deleted)
 
     def add_task(self, task):
         if self.is_task(task):
             self.__tasks.append(task)
+            task_id = task.get_id()
+            task_category = task.get_category()
+            self.create_category(task_category)
+            self.__tasks_ids_in_categories[task_category].append(task_id)
 
-    def get_task_by_name(self, name_of_required_task):
+    #def get_task_by_name(self, name_of_required_task):
+    #    name_of_required_task = str(name_of_required_task).strip()
+    #    for task in self.__tasks:
+    #        task_name = task.get_name()
+    #        if task_name == name_of_required_task:
+    #            return task
+    #    return None
+
+    def get_task_by_id(self, id_of_required_task):
+        print("----------------------")
+        id_of_required_task = str(id_of_required_task).strip()
+        print('get task by id ' + id_of_required_task)
         for task in self.__tasks:
-            task_name = task.get_name()
-            if task_name == name_of_required_task:
+            task_id = task.get_id()
+            print('task ' + task.get_name() + " task id " + task_id)
+            if task_id == id_of_required_task:
                 return task
         return None
 
-    def remove_task_by_name(self, name_of_required_task):
-        task = self.get_task_by_name(name_of_required_task)
+    def remove_task_by_id(self, id_of_required_task):
+        print('Remove task by id: ' + id_of_required_task)
+
+        id_of_required_task = str(id_of_required_task).strip()
+        task = self.get_task_by_id(id_of_required_task)
         if self.is_task(task):
+            print('Obj is a task ')
+            task_id = task.get_id()
+            task_category = task.get_category()
             self.__tasks.remove(task)
+            self.__tasks_ids_in_categories[task_category].remove(task_id)
+
+    #def remove_task_by_name(self, name_of_required_task):
+    #    name_of_required_task = str(name_of_required_task).strip()
+    #    task = self.get_task_by_name(name_of_required_task)
+    #    if self.is_task(task):
+    #        task_name = task.get_name()
+    #        task_category = task.get_category()
+    #        self.__tasks.remove(task)  # removing task from tasks list
+    #        self.__tasks_ids_in_categories[task_category].remove(task_name)  # removing task from list tasks in category
+    #        # todo? add checking is task list in this category empty, and maybe delete this category
 
     def get_tasks(self):
         return self.__tasks
 
-    def mark_task_done(self, name_of_required_task):
-        task = self.get_task_by_name(name_of_required_task)
+    def mark_task_done(self, id_of_required_task):
+        id_of_required_task = str(id_of_required_task).strip()
+        task = self.get_task_by_id(id_of_required_task)
         if self.is_task(task):
             task.mark_done()
 
-    def mark_task_undone(self, name_of_required_task):
-        task = self.get_task_by_name(name_of_required_task)
+    #def mark_task_done(self, name_of_required_task):
+    #    name_of_required_task = str(name_of_required_task).strip()
+    #    task = self.get_task_by_name(name_of_required_task)
+    #    if self.is_task(task):
+    #        task.mark_done()
+
+    def mark_task_undone(self, id_of_required_task):
+        id_of_required_task = str(id_of_required_task).strip()
+        task = self.get_task_by_id(id_of_required_task)
         if self.is_task(task):
            task.mark_undone()
 
-    def set_task_description(self, name_of_required_task, new_description):
-        task = self.get_task_by_name(name_of_required_task)
-        if self.is_task(task):
-            task.set_description(new_description)
+    #def mark_task_undone(self, name_of_required_task):
+    #    name_of_required_task = str(name_of_required_task).strip()
+    #    task = self.get_task_by_name(name_of_required_task)
+    #    if self.is_task(task):
+    #       task.mark_undone()
 
-    def set_task_name(self, name_of_required_task, new_name):
-        task = self.get_task_by_name(name_of_required_task)
+    def set_task_name(self, id_of_required_task, new_name):
+        id_of_required_task = str(id_of_required_task).strip()
+        task = self.get_task_by_id(id_of_required_task)
         if self.is_task(task):
             task.set_name(new_name)
 
-    # добавить обработку ошибок
+    #def set_task_name(self, name_of_required_task, new_name):
+    #    name_of_required_task = str(name_of_required_task).strip()
+    #    task = self.get_task_by_name(name_of_required_task)
+    #    if self.is_task(task):
+    #        task.set_name(new_name)
+    #        task_category = task.get_category()
+    #        self.__tasks_ids_in_categories[task_category].remove(name_of_required_task)
+    #        self.__tasks_ids_in_categories[task_category].append(new_name)
+
+    #def set_task_description(self, name_of_required_task, new_description):
+    #    name_of_required_task = str(name_of_required_task).strip()
+    #    task = self.get_task_by_name(name_of_required_task)
+    #    if self.is_task(task):
+    #        task.set_description(new_description)
+
+    def set_task_description(self, id_of_required_task, new_description):
+        id_of_required_task = str(id_of_required_task).strip()
+        task = self.get_task_by_id(id_of_required_task)
+        if self.is_task(task):
+            task.set_description(new_description)
+
+    # todo: add error handling
+    # todo? add writing category list, for opportunity of existing empty category
     def write_tasks_in_file(self, filename='tasks.txt'):
         data = []
         for task in self.__tasks:
@@ -102,29 +270,52 @@ class Notebook:
         file.close()
         return True
 
-    # Добавить обработку ошибок
+    # todo: Add error handling
+    # todo? add reading category list, for opportunity of existing empty category
     def read_tasks_from_file(self, filename='tasks.txt'):
         file = open(filename, mode='r', encoding='utf-8')
         data = json.load(file)
         file.close()
 
+        if data == "":
+            return True
+
         for task_info in data:
+            task_id = task_info.get('id')
             task_name = task_info.get('name')
             task_description = task_info.get('description')
             task_is_done = task_info.get('is_done')
-            self.add_task(Task(name=task_name, description=task_description, is_done=task_is_done))
+            task_category = task_info.get('category')
+            task = Task(name=task_name, description=task_description,
+                        is_done=task_is_done, category=task_category)
+            task.set_id(task_id)
+            self.add_task(task)
         return True
+
+    def show_notebook(self):
+        print('-------------------begin of notebook--------------')
+        counter = 0
+        for category in self.__categories_names:
+            print('-------------' + category + '------------------')
+            tasks = self.get_tasks_from_category(category)
+            counter += len(tasks)
+            for task in tasks:
+                print(task.show_task())
+        print("\ncount of tasks in notebook: " + str(len(self.__tasks)) +
+              "\ncounter: " + str(counter))
+        print("-------------------end of notebook----------------")
 
 
 if __name__ == "__main__":
     notebook = Notebook()
 
+    print('-------------------1----------------------')
+
     for i in range(0, 10):
-        notebook.add_task(Task(name='Task {number}'.format(number=i),
-                               description='It`s {number} task.'.format(number=i)))
-    tasks = notebook.get_tasks()
+        notebook.add_task(Task(name='Task {number}'.format(number=str(i)),
+                               description='It`s {number} task.'.format(number=str(i))))
+        time.sleep(2)
 
-    for task in tasks:
-        print(task.show_task())
-
-    notebook.write_tasks_in_file()
+    notebook.create_category('Test')
+    test_task_1 = Task(name = 'Test task one', description= 'Tests it is important part of developing', category='  Test ')
+    notebook.show_notebook()
